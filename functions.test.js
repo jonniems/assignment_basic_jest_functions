25a6033cc1e73784373868c9fdb0ff1af61d2731
Jonnie Muis <jonnie.muis@gmail.com>
2020-12-16 16:05:28 +0100

    Initial Commit

---
+++ functions.test.js
@@ -0,0 +1,28 @@
+const functions = require('./functions.js');
+
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
+
+test('checkValue Should be falsy when argument is undefined', () => {
+    expect(functions.checkValue(undefined)).toBeUndefined();
+});
+
+test('User should be Brad Traversy object', () => {
+    expect(functions.createUser()).toEqual({ firstName: 'Brad', lastName: 'Traversy' })
+});
+
+test('Should be under or equal to 1600', () => {
+    const load1 = 800;
+    const load2 = 800;
+    expect(load1 + load2).toBeLessThanOrEqual(1600);
+});
+
+test('There is no I in team', () => {
+    expect('team').not.toContain(`I`);
+});
+
+test('Admin should be in usernames', () => {
+    usernames = ['john', 'karen', 'admin'];
+    expect(usernames).toContain('admin');
+});
